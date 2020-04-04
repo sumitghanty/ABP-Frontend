4e766a2593436e2f0214ab6560dc106beb3d2772
unknown <ganeshd258@gmail.com>
2020-04-04 22:07:18 +0530

    UI changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CreateTripComponent } from './create-trip/create-trip.component';
-
+import { TripListComponent } from './trip-list/trip-list.component';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'createTrip', component: CreateTripComponent }
+  { path: 'createTrip', component: CreateTripComponent },
+  { path: 'tripList', component: TripListComponent }
 ];
 
 @NgModule({
