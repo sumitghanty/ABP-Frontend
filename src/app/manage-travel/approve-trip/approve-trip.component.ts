8562d22ef2b848f785e410fa586f0c1a3a56dde3
chandrabapi <chandrabapi@gmail.com>
2020-04-24 01:37:28 +0530

    up

--- src/app/manage-travel/approve-trip/approve-trip.component.ts
+++ src/app/manage-travel/approve-trip/approve-trip.component.ts
@@ -13,21 +13,47 @@ export class ApproveTripComponent implements OnInit {
   token: any;
   flightList: any;
   hotelList: any;
+  toCityName: any;
+  fromCityName: any;
+  tripForDesc: any;
+  purposeDesc: any;
+  userLoginData: any;
 
   constructor(
     private _createTripService:CreateTripService
   ) { }
 
   ngOnInit() {
-    let userLoginData = JSON.parse(localStorage.getItem('userLoginData'));
-    this.emailId = userLoginData.login.userEmail;
-    this.token = userLoginData.login.generatedToken;
+    this.userLoginData = JSON.parse(localStorage.getItem('userLoginData'));
+    this.emailId = this.userLoginData.login.userEmail;
+    this.token = this.userLoginData.login.generatedToken;
 
     this.tripNumber = localStorage.getItem('detailstripNumber');
     const param = {"tripNumber":this.tripNumber};
     this._createTripService.tripDetailsByNumber(this.emailId,this.token,param).subscribe((res: any) => {
       console.log(res);
       this.tripListData = res.tripcreation;
+
+      // from city name
+      debugger
+      let getCity = this.userLoginData.masterResponseModel.cityMaster;
+      let toCityArr= getCity.filter(ele => ele.cityid == this.tripListData.toLocation)
+      this.toCityName = toCityArr[0].cityName;
+      
+      //to city name
+      let fromCityArr = getCity.filter(ele => ele.cityid == this.tripListData.fromLocation)
+      this.fromCityName = fromCityArr[0].cityName;
+
+      //trip for 
+      let tripForMaster = this.userLoginData.masterResponseModel.tripForMaster
+      let tripForArr = tripForMaster.filter(ele => ele.tripForMasterId == this.tripListData.tripFor);
+      this.tripForDesc = tripForArr[0].tripForMasterDescription;
+      
+      // pupose master
+      let purposeMaster = this.userLoginData.masterResponseModel.purposeMaster
+      let purposeSelectedArr = purposeMaster.filter(ele => ele.purposeId == this.tripListData.purpose);
+      this.purposeDesc = purposeSelectedArr[0].purposeDescription;
+
       this.flightList = res.tripcreation.lineItemMaster[0].flightOrTrainBooking;
       this.hotelList = res.tripcreation.lineItemMaster[0].hotelGuestHouseBookingDetaills;
       console.log('this.tripListData',this.tripListData);
